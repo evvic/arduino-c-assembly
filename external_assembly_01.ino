7ac82af37cb01ea03f57085a094229a831b7f7eb
Eric Brown <ewic98@gmail.com>
2024-09-13 00:19:23 -0700

    init external_assembly_01.ino

---
+++ external_assembly_01.ino
@@ -0,0 +1,45 @@
+#include <Arduino.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <avr/pgmspace.h>
+
+using namespace std;
+
+
+// External functions from assembly
+// Declaring as "C" functions instead of C++ due to C++ trying to give the functions unique names
+extern "C"
+{
+  int16_t Multiply(int16_t x, int16_t y);
+  void DigitalSetASM(int8_t port, int8_t bit);
+  void DigitalClrASM(int8_t port, int8_t bit);
+  void TurnLEDOn();
+  void TurnLEDOff();
+  void SetLED(int8_t state);
+  int8_t PrimeNth(int8_t number);
+  int16_t PlayerFitness();
+}
+
+char stringBuffer[80];
+
+volatile int16_t x = 2;
+volatile int16_t y = 5;
+
+void setup() {
+  // initialize serial port
+  Serial.begin(115200);
+  Serial.print("\n\rIn Setup()...\n\r");
+
+}
+
+void loop() {
+
+  TurnLEDOn();
+  delay(250);
+
+  TurnLEDOn();
+  delay(250);
+
+}
